e5dab832c2fc762ee7569bc48175d4ba38f33d59
Travis Fox <travis.fox90@gmail.com>
2020-07-19 02:29:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  @items = []
+  @items_with_price = {}
+  
+  def initialize(discount= 20)
+    @total = 0
+    @discount = discount
+    self.apply_discount
+  end
+  
+  
+  def add_item(title, price, qty=1)
+    @total += (price * qty)
 
+=begin    
+    @items.push(title * qty)
+    if @items_with_price.include?(title)
+      @items_with_price[title] = @items_with_price[title] += qty
+    else
+      @items_with_price[title] = qty
+    end
+=end
+  end
+  
+  def apply_discount
+    @total = @total * ((100 - @discount).to_f/100)
+    "After the discount, the total comes to $#{@total.to_i}."
+  end
+  
+end
